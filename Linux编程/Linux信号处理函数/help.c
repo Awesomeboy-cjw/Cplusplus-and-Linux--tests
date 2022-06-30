e5ad36676c772b7d81377588587c9233fb74bf87
Awesomeboy-cjw <75939985+Awesomeboy-cjw@users.noreply.github.com>
2022-06-30 12:19:57 +0800

    Update help.c

--- Linux编程/Linux信号处理函数/help.c
+++ Linux编程/Linux信号处理函数/help.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    //给sighandle.cpp运行起来的进程发送SIGHUP信号
+    kill(n, SIGHUP);
+    return 0;
+}
